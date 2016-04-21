0678195c459e83e3b748e14db0db457a999364ae
Jesse Slim <jesse.g.slim@gmail.com>
2016-04-21 22:31:03 +0200

    Met plotjes + C++ code (superslecht idee)

---
+++ comp-phys-lb/comp-phys-lb/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  comp-phys-lb
+//
+//  Created by Jesse Slim on 21/04/16.
+//  Copyright © 2016 Jesse Slim. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
